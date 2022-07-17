f5cd35d46a1fbce3fdd813413dfcf5484bb2d113
Filipe <filipe-all@hotmail.com>
2022-07-17 14:36:48 -0300

    first commit

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://Kanna:Venat69@urlshortener.mf477.mongodb.net/?retryWrites=true&w=majority'
+};
+//# sourceMappingURL=Constants.js.map
